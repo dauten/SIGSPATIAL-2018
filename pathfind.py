31e23a3202162160a84c1bb95f3266e0b4640023
Dale Auten <dauten96@gmail.com>
2018-12-01 12:42:45 -0600

    fixed up BST, gets path

--- pathfind.py
+++ pathfind.py
@@ -26,6 +26,7 @@ class Edge:
 			self.toY = ls[1]
 
 		self.parent = None
+		self.visited = False
 
 	def getSourceName(self):
 		return self.fromX+self.fromY
@@ -36,8 +37,11 @@ class Edge:
 	def __str__(self):
 		return self.getSourceName() + "|" + self.getDestName()
 
+sources = ["6815946.038261843672.78242", "6815957.268881843079.85972", "6815953.440481843350.4143"]
+sinks = ["6815718.619431843338.92843", "6815975.135641842907.57266"]
 
 
+#begin Input
 cords = open("zzzout.txt", "r")
 pmap = {}
 masterlist = []
@@ -62,7 +66,7 @@ for line in cords:
 		t = pmap[temp.getSourceName()]
 		t.append(temp)
 		pmap[temp.getSourceName()] = t
-
+#end Input
 
 #	print("for the record pmap.keys looks like: "+str(pmap.keys())+"\n\n\n")
 
@@ -71,31 +75,46 @@ for line in cords:
 #   destName of destination
 #Q: boolean of if path exists
 def BFS(edgeList, source, destination):
+	head = None
 	q = queue.Queue()
-	q.put(source)	#list of sources
-	visited = []
+
+	if edgeList[source][0] == None:
+		print("no path exists")
+		sys.exit(0)
+	q.put(edgeList[source][0])
+	path = []
+
 	while not q.empty():
-		temp = q.get()
-		if temp not in visited:
-			visited.append(temp)
-			if temp in edgeList.keys():
-				for dest in edgeList[temp]:
-					q.put(dest.getDestName())
-					dest.parent = temp
-					if dest.getDestName() == destination:
-						return True
-				
+		head = q.get()
+		for edge in edgeList[head.getDestName()]:
+			if not edge.visited:
+				edge.visited = True
+				q.put(edge)
+				edge.parent = head
+				if edge.getDestName() == destination and len(path) == 0:
+					path.append(edge)
 
-	print(len(visited))
+	if len(path) == 0:
+		print("no path exists")
+	else:
+		while path[len(path)-1].getSourceName() != edgeList[source][0].getSourceName():
+			path.append(path[len(path)-1].parent)
 
-	return False
+	for esses in edgeList.keys():
+		for edge in edgeList[esses]:
+			edge.parent = None
+			edge.visited = None
 
-for key in pmap.keys():
-	print(pmap[key])
-source = "6815946.038261843672.78242"
-sink = "6815718.619431843338.92843"
-print(BFS(pmap, source, sink))
-
+	return path
 
 
 
+
+
+for p in sources:
+	for q in sinks:
+		print("\n"+p + " to "+q+" is:")
+		out = BFS(pmap, p, q)
+		
+		for e in out:
+			print(e.fromX+", "+e.fromY+" -> "+e.toX+", "+e.toY)
